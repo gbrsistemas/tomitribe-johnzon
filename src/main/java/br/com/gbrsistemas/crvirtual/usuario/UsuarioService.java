ec3c65e420ddeda191f1a609f6606de859a93f28
Ivan Junckes Filho <ivanjunckes@gmail.com>
2020-06-22 11:28:04 -0300

    add url issue

--- src/main/java/br/com/gbrsistemas/crvirtual/usuario/UsuarioService.java
+++ src/main/java/br/com/gbrsistemas/crvirtual/usuario/UsuarioService.java
@@ -5,6 +5,7 @@ import br.com.gbrsistemas.crvirtual.Resultado;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -16,6 +17,20 @@ public class UsuarioService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUsuarios() {
+        List<Usuario> users = new ArrayList<>();
+        users.add(new Usuario(1, "Rafael Guimarães"));
+        users.add(new Usuario(2, "Ivan Junckes Filho"));
+
+        Resultado<Usuario> userResult = new Resultado<>();
+        userResult.setItens(users);
+        userResult.setTotal(Long.valueOf(users.size()));
+        return Response.ok(userResult).build();
+    }
+
+    @Path("url_test")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUsuarios(@QueryParam("filter") String filter) {
         List<Usuario> users = new ArrayList<>();
         users.add(new Usuario(1, "Rafael Guimarães"));
         users.add(new Usuario(2, "Ivan Junckes Filho"));
